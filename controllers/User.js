03be77e0aaa3981d3e8f921355ebd1d49cebc127
Vanraj Khunti <34185015+vanrajk@users.noreply.github.com>
2024-12-06 15:50:32 +0530

    Update User.js

--- controllers/User.js
+++ controllers/User.js
@@ -28,7 +28,7 @@ class UserController extends BaseController {
         try {
             const authHeader = req.headers.authorization;
             if (!authHeader || !authHeader.startsWith('Bearer ')) {
-                return res.status(401).json({ message: 'Authorization token missing or invalid' });
+                return res.status(401).json({ message: 'Authorization token missing or invalid!!!' });
             }
     
             const token = authHeader.split(' ')[1];
